fb4ac9263d20c9f33673d6e00a698901b30f4681
Debojit Das <debojit666@gmail.com>
2020-03-25 02:06:07 +0100

    double equal and triple equal

---
+++ double-vs-tripleEqual.js
@@ -0,0 +1,18 @@
+
+// double equal = only value will be checked
+//triple equal = value and type both with be checked
+const first = 2;
+const second = "2";
+if (first == second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+if (first === second) {
+    console.log("condition is true");
+}
+else {
+    console.log("condition is false");
+}
