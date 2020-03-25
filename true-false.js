86f081e421c27af0f69b62aaad1be7daaf31bfb4
Debojit Das <debojit666@gmail.com>
2020-03-25 01:28:21 +0100

    Truthy and Falsy values

---
+++ true-false.js
@@ -0,0 +1,21 @@
+const age = -4; // value + ba - jai hok condition true hobe. shudhumatro zero hoile condition false hobe.
+
+if(age){
+    console.log("condition is true.");
+}
+else{
+    console.log("condition is false.");
+}
+
+const name = "Solaiman khan";//string er moddhe kisu lekha thaklei truth. kisu lekha na thakle false.
+//white space thakleo sheta true hobe
+if (name.length){
+    console.log("condition is true.");
+}
+else {
+    console.log("condition is false.");
+}
+
+//changeable variable maane let er value na dile sheta undefined, so condition false
+// False list - 0, "",undefined, null, NaN(Not a number),false
+//Truth list - '0', ' ', []eta array, {}eta object
