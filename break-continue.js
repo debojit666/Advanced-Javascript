73ee942455a1cb4456b5109a4e57649a38912ab6
Debojit Das <debojit666@gmail.com>
2020-04-27 05:06:12 +0200

    updated array#

--- break-continue.js
+++ break-continue.js
@@ -1,5 +1,5 @@
 //using break
-const nums = [1,2,3,4,5,6,7,8,9];
+const nums = [1,2,3,4,5,6,7,8,9,10];
 for (let i = 0; i < nums.length; i++){
     console.log(nums[i]);
     if(nums[i]>6){
@@ -7,7 +7,7 @@ for (let i = 0; i < nums.length; i++){
     }
 }
 //using continue
-const nums1 = [1, -2, 3, 4,-5, -6, 7, -8, 9];
+const nums1 = [1, -2, 3, -4,-5, -6, 7, -8, 9];
 for (let i = 0; i < nums1.length; i++) {
     if (nums1[i] < 0) {
         continue;
